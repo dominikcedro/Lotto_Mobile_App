ca30d28d40fee12eefc4e5cc94bc28e66d730dc1
Domin C <dominikcedro00@gmail.com>
2023-11-27 02:34:30 +0100

    moment of spannable string

--- app/src/main/java/com/example/lottoapp4/LoginActivity.kt
+++ app/src/main/java/com/example/lottoapp4/LoginActivity.kt
@@ -1,11 +1,48 @@
 package com.example.lottoapp4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val toRegisterText = findViewById<TextView>(R.id.toRegisterText)
+        val fullText = "Not a registered user? Sign up"
+        val clickableWord = "Sign up"
+        val spannableString = SpannableString(fullText)
+        val startIndex = fullText.indexOf(clickableWord)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        spannableString.setSpan(
+            clickableSpan,
+            startIndex,
+            startIndex + clickableWord.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        toRegisterText.text = spannableString
+        toRegisterText.movementMethod = LinkMovementMethod.getInstance()
+        val buttonLogin = findViewById<Button>(R.id.LoginButton)
+
+        buttonLogin.setOnClickListener {
+
+
+            val intent = Intent(this, NumbSelectionActitivy::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/lottoapp4/NumbSelectionActitivy.kt
@@ -0,0 +1,11 @@
+package com.example.lottoapp4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NumbSelectionActitivy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numb_selection_actitivy)
+    }
+}
